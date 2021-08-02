058837b3e694bc0ea3f3287f529f6d8b0fc9acbc
Holasz Moni <holaszmonika@gmail.com>
2021-08-02 17:36:49 +0300

    Updates

--- src/test/java/com/pages/DashboardPage.java
+++ src/test/java/com/pages/DashboardPage.java
@@ -8,7 +8,14 @@ public class DashboardPage extends BasePage{
     @FindBy(css = ".hello strong")
     private WebElementFacade welcomeMsg;
 
+    @FindBy(css = ".success-msg span")
+    private WebElementFacade confirmationMessage;
+
     public boolean isWelcomeMsg(String text){
         return welcomeMsg.containsOnlyText("Hello, " + text + "!");
     }
+    public boolean isConfirmationMessage(String value){
+        return confirmationMessage.containsElements("Thank you for registering with Madison Island.");
+    }
+
 }
