7f340fa261696d33ad65b9262e7fbf2ba76de00f
Holasz Moni <holaszmonika@gmail.com>
2021-08-03 22:39:50 +0300

    updating gitignore and constants

--- src/test/java/com/Utils/Constants.java
+++ src/test/java/com/Utils/Constants.java
@@ -1,7 +1,14 @@
 package com.Utils;
 
+import org.yecht.Data;
+
 public class Constants {
+    public static final String URL = "http://qa2.dev.evozon.com/";
     public static final String EMAIL = "ab@yahoo.com";
     public static final String PASS = "aaaaaa";
     public static final String USER_NAME= "aadsa asda";
+
+    public static  final String USER_TO_UPDATE_PASSWORD = "eminem";
+    public static  final String USER_TO_UPDATE_USERNAME = "moni@yahoo.com";
+
 }
